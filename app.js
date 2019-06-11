864124af1888bfb270c3da75a04ee11b98b38170
Tarek-Zaher <Tarek21Zaher@gmail.com>
2019-06-10 19:20:33 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//jshint esversion: 6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server running on port 3000");
+});
