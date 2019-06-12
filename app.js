8ed905cffa49037346d0bd72d7ebae9f216f8c0d
Tarek-Zaher <Tarek21Zaher@gmail.com>
2019-06-12 12:47:46 -0500

    build mobile and desktop versions and code foundation

--- app.js
+++ app.js
@@ -10,8 +10,83 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const defaultPoem = "hello friend,&#10;thanks for coming to my site!&#10;you didn't fill out the form&#10;but that's alright&#10;&#10;I hope you'll go back&#10;and fill it out all the way&#10;and then send it to someone special&#10;and make someone special's day. (:&#10;&#10;";
+const motherGeneralThanks = "When I was a baby&#10;You made sure that I grew&#10;and now I think, maybe,&#10;I would like to thank you:&#10;&#10;Thanks! (:&#10;&#10;";
+const motherBirthday = "It is your birthday and&#10;this is a test&#10;&#10;";
+const motherRomantic = "Freud must be smiling so bright&#10;if, now, he looks upon me&#10;for his theories—they are right&#10;I'm in love with my mommy.&#10;&#10;";
+
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.post("/", (req, res) => {
+  const person = req.body.person;
+  const mood = req.body.mood;
+  const name = req.body.name;
+
+  switch (person) {
+    case "mother":
+      if (mood === "general-thanks") {
+        res.render("poem", {poem: motherGeneralThanks, name: name, lines: 8});
+      }
+      if (mood === "birthday") {
+        res.render("poem", {poem: motherBirthday, name: name, lines: 4});
+      }
+      if (mood === "romantic") {
+        res.render("poem", {poem: motherRomantic, name: name, lines: 6});
+      }
+      if (mood === "anger") {
+        res.render("poem", {poem: "This poem needs to be written", name: name, lines: 6});
+      }
+      break;
+    case "father":
+      if (mood === "general-thanks") {
+        res.render("poem", {poem: "This poem needs to be written", name: name, lines: 6});
+      }
+      if (mood === "birthday") {
+        res.render("poem", {poem: "This poem needs to be written", name: name, lines: 6});
+      }
+      if (mood === "romantic") {
+        res.render("poem", {poem: "This poem needs to be written", name: name, lines: 6});
+      }
+      if (mood === "anger") {
+        res.render("poem", {poem: "This poem needs to be written", name: name, lines: 6});
+      }
+      break;
+    case "significant-other":
+      if (mood === "general-thanks") {
+        res.render("poem", {poem: "This poem needs to be written", name: name, lines: 6});
+      }
+      if (mood === "birthday") {
+        res.render("poem", {poem: "This poem needs to be written", name: name, lines: 6});
+      }
+      if (mood === "romantic") {
+        res.render("poem", {poem: "This poem needs to be written", name: name, lines: 6});
+      }
+      if (mood === "anger") {
+        res.render("poem", {poem: "This poem needs to be written", name: name, lines: 6});
+      }
+      break;
+    case "friend":
+      if (mood === "general-thanks") {
+        res.render("poem", {poem: "This poem needs to be written", name: name, lines: 6});
+      }
+      if (mood === "birthday") {
+        res.render("poem", {poem: "This poem needs to be written", name: name, lines: 6});
+      }
+      if (mood === "romantic") {
+        res.render("poem", {poem: "This poem needs to be written", name: name, lines: 6});
+      }
+      if (mood === "anger") {
+        res.render("poem", {poem: "This poem needs to be written", name: name, lines: 6});
+      }
+      break;
+    default:
+      res.render("poem", {poem: defaultPoem, name: "Tarek", lines: 11});
+
+  }
+
+  res.render("poem", {poem: motherGeneralThanks, name: name});
 });
 
 
---
+++ public/js/copyPoem.js
@@ -0,0 +1,13 @@
+function copyPoem() {
+  var copyText = document.getElementById("poem");
+  copyText.select();
+  document.execCommand("copy");
+
+  var alert = document.createElement("div");
+   alert.setAttribute("style","position:fixed;width:50%;height:15%;top:calc(75% / 2);left:25%;background-color:#3D3D3D;color:white;border-radius:25px");
+   alert.innerHTML = "<h2 class='copied'>Copied! 🙂</h1>";
+   setTimeout(function(){
+    alert.parentNode.removeChild(alert);
+  }, 1000);
+   document.body.appendChild(alert);
+}
